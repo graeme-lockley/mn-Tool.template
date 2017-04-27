58e6fb68d995d83196bfc8687b9dbaae010d457e
Graeme Lockley <graemel@no9.co.za>
2017-04-27 07:42:09 +0200

    refactor: make use of functions from Native.FileSystem

--- Test/TemplateToolTest.js
+++ Test/TemplateToolTest.js
@@ -1,6 +1,5 @@
 const Assert = require("assert");
-const FileSystem = mrequire("core:System.IO.Native.FileSystem:1.0.0");
-const FS = require("fs");
+const FileSystem = mrequire("core:System.IO.Native.FileSystem:1.1.0");
 const Result = mrequire("core:Data.Result:1.0.0");
 const Unit = mrequire("core:Test.Unit:v1.0.0");
 
@@ -9,40 +8,16 @@ const Use = require("../index.js");
 const Translate = require("../Tool/Template/Translate");
 
 
-const suite = Unit.newSuite("Text Template Suite");
-
-
-const denodeify = proc =>
-    new Promise((fulfill, reject) => {
-        proc((err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                fulfill(result);
-            }
-        });
-    });
-
-
-const open = fileName => options =>
-    denodeify(cb => FS.open(fileName, options, cb));
-
-
-const futimes = atime => mtime => fileDescriptor =>
-    denodeify(cb => FS.futimes(fileDescriptor, atime, mtime, cb));
-
-
-const close = fileDescriptor =>
-    denodeify(cb => FS.close(fileDescriptor, cb));
-
-
 const touch = fileName => {
     const now = new Date();
 
-    return open(fileName)("r+")
-        .then(fd => futimes(now)(now)(fd)
-            .then(close(fd)));
+    return FileSystem.open(fileName)("r+")
+        .then(fd => FileSystem.futimes(now)(now)(fd)
+            .then(FileSystem.close(fd)));
 };
+
+
+const suite = Unit.newSuite("Text Template Suite");
 
 
 Promise.all([
